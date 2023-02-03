f40fd6a4824c2737512dd99a1b1e5ab2839560dd
Y M V A D <fgost31@gmail.com>
2023-02-04 00:59:38 +0300

    TMS HW 6 and 10 were added

---
+++ TMS10.py
@@ -0,0 +1,23 @@
+class MyException(Exception):
+    def __init__(self, *args):
+        self.message = args[0] if args else None
+    def __str__(self):
+        return f"X> ошибка определенного класса {self.message}"
+
+
+cont = True
+while(cont):
+    prompt = input("Введите числовое выражение:\n")
+    try:
+        if prompt == 'NaN' :
+            raise MyException(prompt)
+        print(eval(prompt))
+    except MyException:
+        print(MyException(prompt))
+    except SyntaxError:
+        print("X> синтаксическая ошибка")
+    except NameError:
+        print("X> переменная не обьявлена")
+    finally:
+        prompt = input('продолжить? [y] [n]')
+        cont = True if (prompt == 'y') else False
